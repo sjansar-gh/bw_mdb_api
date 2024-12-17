de515f53241929b0047f176f4301d0076fbc135a
Javed Syed <sjansar@gmail.com>
2024-12-17 15:02:41 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT | 8000
+
+app.get("/", (req, res) => {
+    return res.status("200").json({
+        msg: 'GET api'
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`bw_api server listening on ${PORT}`);
+});
